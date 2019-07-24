f92fedec87c0882683dcc5c424a2bbb4f86cb9b7
shivam1886 <33111444+shivam1886@users.noreply.github.com>
2019-07-24 18:59:26 +0530

    Update getLatAndLand.php

--- getLatAndLand.php
+++ getLatAndLand.php
@@ -1,44 +1,25 @@
 <?php
 
-  function toUser($device_token,$data = array()){
-   
-    $registration_ids =  $device_token;
-    $message = array(
-      "message" => $data
-    );
-  
-    $url = 'https://fcm.googleapis.com/fcm/send';
-    $fields = array(
-    'registration_ids' => $registration_ids,
-    'data' => $message,
-    );
-    $headers = array(
-    'Authorization: key=' . "AAAAORSizbQ:APA91bEveAbKk5JvuUsWXYmpucla7kMCdi6VY1ZWR0igOAimVC4Iqw6onTJJy98JPEl8n5HDDxFAgeSzNOPSPybQwaM_YEqc5J0y8HT4t5e5rHZlXLSml-W0B6TNAnU7VKN-Mi0iWYmV",
-    'Content-Type: application/json'
-    );
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-    $result = curl_exec($ch);
-    if ($result === FALSE)
-      {
-      die('Curl failed: ' . curl_error($ch));
-      }
-    curl_close($ch);
+function get_latlng($address) {
+        $address = urlencode(trim($address));
+        $details_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" . $address . "&sensor=false&key=AIzaSyAGQC7r17YyESlAGS8raZ0G1Q-r9Q1s4Vk";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $details_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = json_decode(curl_exec($ch), true);
+        if ($response['status'] != 'OK') {
+            return null;
+        }
+        $latLng = $response['results'][0]['geometry']['location'];
+        return $latLng;
     }
-
-    $data = array(
-      'title' => 'hello Sir',
-      'body'  => 'hello Shir kaiso ho',
-    );
-
-    $token = array('f106gwPTeP0:APA91bFBnEDORIW921Vdivl0gKuFkhs8FGrIuZC3REpr7aWq7eREK0mLWCDS_YLFcKqfWW-DR4S1rafseHiVUIIxoI-YlgGixt5gtb5vWHFNclkPjwVStYhFmVqqjquPQd4L3TfLgfIj');
-
-    toUser($token,$data);
-
+    
+    $search = $_GET['search'];
+    
+    if(empty($search)){
+       $search = 'Indore';
+    }
+    $response = get_latlng($search);
+    print_r($response);
 ?>
