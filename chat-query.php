6bf1d1ae48b6e98473354b79075be580cce70d12
shivam1886 <33111444+shivam1886@users.noreply.github.com>
2020-11-10 12:19:08 +0530

    Update chat-query.php

--- chat-query.php
+++ chat-query.php
@@ -1,17 +1,1 @@
-SELECT t1.*
-        FROM chats AS t1
-        INNER JOIN
-        (
-            SELECT
-                LEAST(sender_id, receiver_id) AS sender_id,
-                GREATEST(sender_id, receiver_id) AS receiver_id,
-                MAX(id) AS max_id
-            FROM chats
-            GROUP BY
-                LEAST(sender_id, receiver_id),
-                GREATEST(sender_id, receiver_id)
-        ) AS t2
-            ON LEAST(t1.sender_id, t1.receiver_id) = t2.sender_id AND
-               GREATEST(t1.sender_id, t1.receiver_id) = t2.receiver_id AND
-               t1.id = t2.max_id
-            WHERE t1.sender_id = 44 OR t1.receiver_id = 44
+SELECT t1.id as chat_id , t1.ad_id , IF(sender_id = $user_id , receiver_id , sender_id ) as user_id , users.name as user_name , users.profile_image , users.is_online , users.last_seen , ads.title , t1.message , ad_images.name as ad_image , t1.created_at as time FROM `chats` as t1 INNER JOIN (SELECT MAX(id) as max_id FROM `chats` GROUP BY ad_id) as t2 ON t1.id = t2.max_id JOIN `users` ON IF(sender_id = 44 , receiver_id , sender_id ) = users.id JOIN `ads` ON t1.ad_id = ads.id JOIN `ad_images` ON t1.ad_id = ad_images.ad_id where t1.sender_id = $user_id OR t1.receiver_id = $user_id ORDER BY t2.max_id DESC
