2dd077048997d0a06b8e75e4140bd34c0d39dd88
shivam1886 <33111444+shivam1886@users.noreply.github.com>
2020-11-20 12:21:18 +0530

    Update HomeController.php

--- HomeController.php
+++ HomeController.php
@@ -18,12 +18,12 @@ class HomeController extends Controller{
         $inputs  = $request->all();
         $rules = [
             'adminEmail'    => 'required|unique:users,email,null,id,deleted_at,NULL',
-            'tenantName'    => 'required|email',
+            'tenantName'    => 'required',
             'password'      => 'required|min:8',
-         //   'planDetails'   => 'required'
+            'planDetails'   => 'required'
         ];
 
-        $validator = Validator::make($inputs, $rules,$message);
+        $validator = Validator::make($inputs, $rules);
         if ($validator->fails()) {
             $errors =  $validator->errors()->all();
             return response(['status' => false , 'message' => $errors[0]] , 200);              
@@ -32,19 +32,35 @@ class HomeController extends Controller{
         $insertData = array(
              'email'    => $inputs['adminEmail'],
              'password' => Hash::make($inputs['password']),
-             'user_name' => $inputs['tenantName']
+             'user_name' => $inputs['tenantName'],
+             'role'      => '3'
          );
+            DB::beginTransaction();
 
-        if(DB::table('users')->insertGetId($insertData)){
-         return ['status'=>true,'message'=>'Successfully registered','data'=>$inputs];
-        }
-        return ['status'=>false,'message'=>'Failed to registered'];
+            try {
+
+                $tenantId = DB::table('users')->insertGetId($insertData);
+                $plandDetails = array(
+                    "tenant_id"   => $tenantId,
+                    'allow_Logo'   => $inputs['planDetails']['allowLogo']   ?? false,
+                    'max_building' => $inputs['planDetails']['maxbuilding'] ?? '0',
+                    'max_office'   => $inputs['planDetails']['maxoffice']   ?? '0',
+                    'max_seats'    => $inputs['planDetails']['maxseats']    ?? '0',
+                );
+                DB::table('tenant_details')->insertGetId($plandDetails);
+                DB::commit();
+                $inputs['tenantID'] = $tenantId; 
+                return ['status'=>true,'message'=>'Successfully registered','data'=>$inputs];
+            } catch (\Exception $e) {
+                DB::rollback();
+                return ['status'=>false,'message'=>'Failed to registered'];
+            }
     } 
 
     public function tenantList(Request $request){
         
-        $tenats = User::select('users.id as tenant_id','users.user_name as tenant_name','users.email as tenant_email')
-                        ->where('role','2')
+        $tenats = User::select('users.id as tanantID','users.user_name as tenantName','users.email as adminEmail')
+                        ->where('role','3')
                         ->where('active_status','1')
                         ->whereNull('deleted_at')
                         ->get();
@@ -56,17 +72,16 @@ class HomeController extends Controller{
     }
 
     public function updateTenant(Request $request){
-        $inputs  = $request->all();
-        $tenant_id = $request->tenant_id;
+        $inputs   = $request->all();
+        $tenantId = $request->tenantID;
         $rules = [
-            'tenant_id'     =>' required',
-            'adminEmail'    => 'required|unique:users,email,'.$tenant_id.',id,deleted_at,NULL',
-            'tenantName'    => 'required|email',
-            'password'      => 'required|min:8',
-         //   'planDetails'   => 'required'
+            'tenantID'      => 'required',
+            'adminEmail'    => 'required|unique:users,email,'.$tenantId.',id,deleted_at,NULL',
+            'tenantName'    => 'required',
+            'planDetails'   => 'required'
         ];
 
-        $validator = Validator::make($inputs, $rules,$message);
+        $validator = Validator::make($inputs, $rules);
         if ($validator->fails()) {
             $errors =  $validator->errors()->all();
             return response(['status' => false , 'message' => $errors[0]] , 200);              
@@ -74,23 +89,35 @@ class HomeController extends Controller{
 
         $updateData = array(
              'email'    => $inputs['adminEmail'],
-             'password' => Hash::make($inputs['password']),
              'user_name' => $inputs['tenantName']
          );
 
-        if(DB::table('users')->where('id',$inputs['tenant_id'])->update($updateData)){
-         return ['status'=>true,'message'=>'Successfully updated','data'=>$inputs];
-        }
-        return ['status'=>false,'message'=>'Failed to update'];
+            DB::beginTransaction();
+
+            try {
+                DB::table('users')->where('id',$tenantId)->update($updateData);
+                $plandDetails = array(
+                    'allow_logo'   => $inputs['planDetails']['allowLogo']   ?? false,
+                    'max_building' => $inputs['planDetails']['maxbuilding'] ?? '0',
+                    'max_office'   => $inputs['planDetails']['maxoffice']   ?? '0',
+                    'max_seats'    => $inputs['planDetails']['maxseats']    ?? '0',
+                );
+                DB::table('tenant_details')->where('tenant_id',$tenantId)->update($plandDetails);
+                DB::commit();
+                return ['status'=>true,'message'=>'Successfully updated','data'=>$inputs];
+            } catch (\Exception $e) {
+                DB::rollback();
+                return ['status'=>false,'message'=>'Failed to update'];
+            }
     }
 
     public function removeTenant(Request $request){
         $inputs  = $request->all();
         $rules = [
-            'tenant_id'     =>' required',
+            'tenantID'     =>' required',
         ];
 
-        $validator = Validator::make($inputs, $rules,$message);
+        $validator = Validator::make($inputs, $rules);
         if ($validator->fails()) {
             $errors =  $validator->errors()->all();
             return response(['status' => false , 'message' => $errors[0]] , 200);              
@@ -100,7 +127,7 @@ class HomeController extends Controller{
              'deleted_at'    => date('Y-m-d H:i:s')
          );
 
-        if(DB::table('users')->where('id',$inputs['tenant_id'])->update($updateData)){
+        if(DB::table('users')->where('id',$inputs['tenantID'])->update($updateData)){
          return ['status'=>true,'message'=>'Successfully removed','data'=>$inputs];
         }
         return ['status'=>false,'message'=>'Failed to remove'];
