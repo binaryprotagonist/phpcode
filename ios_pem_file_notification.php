c418a2e67bce6f0efdb80abf591427b3f3df69d1
shivam1886 <33111444+shivam1886@users.noreply.github.com>
2020-12-08 18:57:45 +0530

    Update ios_pem_file_notification.php

--- ios_pem_file_notification.php
+++ ios_pem_file_notification.php
@@ -1,29 +1,31 @@
- $token = $value; 
-					$title = 'Hello Shiv!;
-					$passphrase='';
-					$Certificate=public_path('certificate.pem');
+<?php
 
-				$ctx = stream_context_create();
-				stream_context_set_option($ctx, 'ssl', 'local_cert', $Certificate);
-				stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+	$token = 'af44ddb179838984128456f62570cf887973f3c746defab9e931c824aeb597d9';
+	
+	$alert = 'Hello Shiv';
+	$passphrase='';
+	$Certificate='certificate.pem';
 
-			//	$fp =stream_socket_client(
-			//	        'ssl://gateway.push.apple.com:2195', $err, $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
-
-				 $fp =stream_socket_client(
-				         'ssl://gateway.sandbox.push.apple.com:2195', $err, $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
+	$ctx = stream_context_create();
+	stream_context_set_option($ctx, 'ssl', 'local_cert', $Certificate);
+	stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
 
 
-				if (!$fp)
-				    exit("Failed to connect: $err $errstr" . PHP_EOL);
+	$fp =stream_socket_client(
+			'ssl://gateway.sandbox.push.apple.com:2195', $err, $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
 
-				$body['aps']=[
-				    'alert' => $title,
-				    'sound' => 'default',
-				    'badge' => 1
-				];
 
-				$payload = json_encode($body);
-				$msg=chr(0).pack('n', 32).pack('H*', trim($token)) . pack('n', strlen($payload)).$payload;
-				$result=fwrite($fp, $msg, strlen($msg));
-				fclose($fp);
+	if (!$fp)
+		exit("Failed to connect: $err $errstr" . PHP_EOL);
+
+	$body['aps']=[
+		'alert' => $alert,
+		'sound' => 'default',
+		'badge' => 1,
+	];
+
+	$payload = json_encode($body);
+	$msg=chr(0).pack('n', 32).pack('H*', trim($token)) . pack('n', strlen($payload)).$payload;
+	$result=fwrite($fp, $msg, strlen($msg));
+	fclose($fp);
+?> 
